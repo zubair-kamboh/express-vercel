e803bc5fa4ebc048ebd9354af69ce638f32997a4
zubair-kamboh <zubairkamboh9010@gmail.com>
2023-02-06 14:59:13 +0500

    added body parser

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/api', async (req, res) => {
   try {
     const { data } = await axios('https://jsonplaceholder.typicode.com/posts')
