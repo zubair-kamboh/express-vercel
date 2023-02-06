0678225af66f0d7534be66c44e069b1aa428fe2c
zubair-kamboh <zubairkamboh9010@gmail.com>
2023-02-06 14:48:04 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const axios = require('axios')
+const app = express()
+
+app.get('/api', async (req, res) => {
+  try {
+    const { data } = await axios('https://jsonplaceholder.typicode.com/posts')
+    res.send(data)
+    return
+  } catch (e) {
+    console.log(e)
+  }
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => console.log(`listening at port: ${port}`))
+
+// Export the Express API
+module.exports = app
